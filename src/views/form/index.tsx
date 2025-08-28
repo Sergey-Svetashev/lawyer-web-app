2bb7110fcb3907b08df14b1127a8008950453382
Sergei Svetashev <sergei.svetashev@rakuten.com>
2025-08-28 20:34:48 +0900

    Introduce BE API, remove Sendsay api dependency | Update styles & amend the form

--- src/views/form/index.tsx
+++ src/views/form/index.tsx
@@ -1,28 +1,16 @@
 import { useStore } from "@nanostores/react";
-import type { FieldError, FieldErrorsImpl, Merge } from "react-hook-form";
+import type { EventHandler, SyntheticEvent } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Button } from "../../components/btn/btn";
 import { state } from "../../state";
 import TEXT from "../../TEXT";
 import "./index.css";
-import Sendsay from "sendsay-api";
-
-const sendsay = new Sendsay({
-  auth: {
-    login: "your_login",
-    password: "your_password",
-  },
-});
-
-const ErrorMessage = ({
-  error,
-}: {
-  error?: FieldError | Merge<FieldError, FieldErrorsImpl<any>>;
-}) =>
-  error && <span className="text-red-700 text-sm">{TEXT.form.required}</span>;
 
 export const Form = () => {
   const context = useStore(state.home);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSendingError, setIsSendingError] = useState<boolean>(false);
 
   const {
     register,
@@ -30,40 +18,45 @@ export const Form = () => {
     formState: { errors },
   } = useForm<{ name: string; email: string; message: string }>();
 
-  const onSubmit = handleSubmit(({ name, email, message }) =>
-    sendsay
-      .request({
-        action: "issue.send",
-        sendwhen: "now",
-        letter: {
-          subject: "Your Subject Here",
-          "from.name": "Sender Name",
-          "from.email": "sender@example.com",
-          message: {
-            html: `
-              <p>Name: ${name}</p>
-              <p>Email: ${email}</p>
-              <p>Message: ${message}</p>
-            `,
-            // Or use 'text' for plain text messages
-          },
-        },
-        users: ["senju-sama@mail.ru"], // the one mailbox to receive email
-      })
-      .then((response) => {
-        console.log("Email sent:", response);
+  const onSubmit = handleSubmit(({ name, email, message }) => {
+    setIsLoading(true);
+
+    fetch("https://deno-sample-be.sergei-svetashev.deno.net/send", {
+      method: "POST",
+      body: JSON.stringify({
+        html: `<h3>${TEXT.form.senderName}: ${name}</h3>
+                <h4>${TEXT.form.senderMail}: ${email}</h4>
+                <p>${TEXT.form.senderMessage}: ${message}</p>
+              `,
+      }),
+    })
+      .then(() => {
+        console.log("Email sent with following data:", name, email, message);
+        setIsLoading(false);
+        state.toggleFormState();
       })
       .catch((error) => {
-        console.error("Error sending email:", error);
-      })
-  );
+        setIsLoading(false);
+        setIsSendingError(true);
+        console.error("An error occurred while sending the email:", error);
+      });
+  });
+
+  const bgClickHandler: EventHandler<SyntheticEvent> = (e) => {
+    if (e.currentTarget === e.target) {
+      state.toggleFormState();
+    }
+  };
 
   return (
-    <div className={`form-bg ${context.isFormOpen ? "open" : ""}`}>
+    <div
+      className={`form-bg ${context.isFormOpen ? "open" : ""}`}
+      onClick={bgClickHandler}
+    >
       <div className="table-cell align-middle">
         <form
-          className="p-8 w-80 rounded bg-white-0.7 relative shadow-black-0.5 mx-auto my-0"
-          onSubmit={onSubmit} // TODO: replace with mail service AHMGPE1XWM5TGAX3S9B4X184
+          className="p-8 w-80 rounded bg-white relative shadow-black-0.5 mx-auto my-0"
+          onSubmit={onSubmit}
           method="post"
         >
           <span className="closeBtn" onClick={state.toggleFormState} />
@@ -86,9 +79,14 @@ export const Form = () => {
           <div className="mb-2.5">
             <input
               className="block w-full p-3.5 border border-solid border-black resize-none font-montserrat"
-              // type="email"
               placeholder={TEXT.form.mail}
-              {...register("email", { required: true })}
+              {...register("email", {
+                required: true,
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
+              })}
             />
             {errors.email && (
               <span className="text-red-700 text-sm">
@@ -100,13 +98,20 @@ export const Form = () => {
             <textarea
               className="block w-full p-3.5 border border-solid border-black resize-none font-montserrat"
               placeholder={TEXT.form.message}
-              {...register("message", { required: true })}
+              {...register("message")}
             />
             {errors.message && (
               <span className="text-red-700 text-sm">{TEXT.form.required}</span>
             )}
           </div>
-          <Button text={TEXT.form.submit} type="submit" />
+          <Button
+            text={isLoading ? TEXT.form.sending : TEXT.form.submit}
+            type="submit"
+            disabled={isLoading}
+          />
+          {isSendingError ? (
+            <p className="py-2 text-red-700 text-xs">{TEXT.form.sandingMailError}</p>
+          ) : null}
         </form>
       </div>
     </div>
